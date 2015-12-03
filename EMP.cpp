7b85c5b48a2d0096314ee7cb6fd9b6a786956e79
JohnHurtTheFool <FryeES1@gcc.edu>
2015-12-03 17:12:21 -0500

    continued work on EMP class

---
+++ EMP.cpp
@@ -0,0 +1,8 @@
+#include "EMP.h"
+EMP::EMP(void)
+{
+
+}
+EMP::~EMP(void)
+{
+}
--- EMP.h
+++ EMP.h
@@ -5,8 +5,20 @@
 #include "entity.h"
 #include "constants.h"
 #include "graphics.h"
+namespace EMPNS
+{
+    const int WIDTH = 100;                   // image width
+    const int HEIGHT = 100;                  // image height
+	const int X = GAME_WIDTH/2 - WIDTH/2;   // location on screen
+    const int Y = GAME_HEIGHT/2 - HEIGHT/2;
+	const float SHIELD_SCALE=0.5;
+   const float maxShieldTime = 15.00f;
+   const int MAX_HITS = 2;
+}
 class EMP :public Entity
 {
+private:
+	float currentTime;
 public:
 	EMP(void);
 	~EMP(void);
